a2c600de35cb964269ed6419186298955d23c9dc
Ben Gardiner <bennygards@gmail.com>
2020-06-29 14:56:19 +1000

    beginning outline of the game

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace muayThaiGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+        }
+    }
+
+    class Fighter
+    {
+        public string firstName;
+        public string lastName;
+        public string weight;
+        public string height;
+        public int power;
+        public int kick;
+        public int punch;
+        public int knee;
+        public int elbow;
+        public int clinch;
+        Random skillGenerator = new System.Random();
+        ////-.-.-.-.-.-.-.-.-.-.-.-.////
+
+        public Fighter()
+        {
+        }
+
+        public void randomSkillGenerator()
+        {
+
+        }
+
+        public string fighterNameGenerator()
+        {
+            return "";
+        }
+
+
+
+
+
+    }
+
+    class Trainer
+    {
+        public string name;
+        public int skillRating;
+
+        ////-.-.-.-.-.-.-.-.-.-.-.-.////    
+
+        public Trainer()
+        {
+        }
+
+        public void trainFighter()
+        {
+
+        }
+
+
+    }
+
+    class Match
+    {
+        public List<Fighter> matchActual; 
+        ////-.-.-.-.-.-.-.-.-.-.-.-.////
+
+        public Match()
+        {
+        }
+
+        
+    }
+
+
+
+
+}   
